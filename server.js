e3a6646c3b9056625de013fad164578b5f2237ea
Pedro <pedroaraujo_41@hotmail.com>
2021-01-28 15:28:36 +0000

    updated route

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.set('view engine', 'ejs');
 
 // routes
-app.use('/', (req, res) => res.send('AR models API'));
+app.get('/', (req, res) => res.send('AR models API'));
 app.use('/armodels', require('./routes/armodels'));
 
 app.listen(PORT, (err) => {
